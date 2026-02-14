54e8bc570d439a6c36e1a129806d5e6ab1eb509c
luputer <m.saidibjm4@gmail.com>
2026-02-15 04:57:44 +0800

    update config error:

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -94,7 +94,7 @@ export const authOptions: NextAuthOptions = {
                     where: { email: credentials.email },
                 });
 
-                if (!user || !user.password) {
+                if (!user?.password) {
                     return null;
                 }
 
