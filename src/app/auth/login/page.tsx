a7aff766f2fb939c77029db6b8e47dfb498703a5
luputer <m.saidibjm4@gmail.com>
2026-02-15 10:16:29 +0800

    update config auth:

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -112,7 +112,7 @@ export default function LoginPage() {
                         <button
                             type="submit"
                             disabled={loading}
-                            className="mt-6 w-full rounded-lg bg-gradient-to-r from-blue-500 to-indigo-500 py-2.5 font-bold text-white shadow-lg transition-transform hover:scale-[1.02] active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed"
+                            className="cursor-pointer mt-6 w-full rounded-lg bg-gradient-to-r from-blue-500 to-indigo-500 py-2.5 font-bold text-white shadow-lg transition-transform hover:scale-[1.02] active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed"
                         >
                             {loading ? "Memproses..." : "Login"}
                         </button>
@@ -132,7 +132,7 @@ export default function LoginPage() {
                     <button
                         type="button"
                         onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-                        className="flex w-full items-center justify-center gap-2 rounded-lg border border-gray-300 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors"
+                        className=" cursor-pointer flex w-full items-center justify-center gap-2 rounded-lg border border-gray-300 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 transition-colors"
                     >
                         <svg className="h-5 w-5" viewBox="0 0 24 24">
                             <path
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -44,6 +44,9 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+    pages: {
+        signIn: "/auth/login",
+    },
     callbacks: {
         jwt: async ({ token, user }) => {
             if (user) {
